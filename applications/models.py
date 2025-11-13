33ff487b6b798726a701b2409de17fca4399416b
Mahmud Hasan <mh.kid234@gmail.com>
2025-11-13 17:19:57 +0600

    corrected applicatons models.py resume blank and null true

--- applications/models.py
+++ applications/models.py
@@ -26,7 +26,7 @@ class Application(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
     applicant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='applications')
     cover_letter = models.FileField(upload_to='cover_letters/', blank=True, null=True)
-    resume = models.FileField(upload_to='resumes/')
+    resume = models.FileField(upload_to='resumes/', blank=True, null=True)
     portfolio_link = models.URLField(blank=True, null=True)
     applied_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=PENDING)
